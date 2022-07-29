ff5b21c70af35fa7c91e2458bcda573b3df13ef4
cwylycode <joeisnomoe@mail.com>
2022-07-28 19:42:31 -0600

    Animation for page header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,4 +1,5 @@
 import { Divider, Flex, Heading } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 interface PageHeaderProps {
   title: string
@@ -11,10 +12,26 @@ export default function PageHeader({ title }: PageHeaderProps) {
       alignItems='center'
       paddingTop='5'
       marginBottom='10'
+      as={motion.div}
+      initial={{ opacity: 0, y: -100 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          opacity: {
+            duration: 2
+          },
+          y: {
+            type: 'spring',
+            stiffness: 50,
+            damping: 2,
+            mass: 0.5,
+            velocity: 100
+          }
+        }
+      }}
     >
-      <Heading>
-        {title}
-      </Heading>
+      <Heading>{title}</Heading>
       <Divider
         marginTop='5'
         borderWidth='1px'
