3f0e75ff7d8ece63c9af67f5d33f7b52fa70748f
cwylycode <joeisnomoe@mail.com>
2022-08-02 23:06:40 -0600

    Removed split text from about page

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,32 +1,12 @@
-import {
-  Box,
-  Button,
-  Container,
-  Flex,
-  Image,
-  Spacer,
-  Text,
-  useBreakpointValue,
-  usePrefersReducedMotion
-} from "@chakra-ui/react";
+import { Box, Button, Container, Flex, Image, Spacer, Text, usePrefersReducedMotion } from "@chakra-ui/react";
 import PageHeader from "../components/PageHeader";
 import pic from '../portrait.jpg'
 import { motion } from "framer-motion";
-import SplitText from "../components/SplitText"
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 import SvgDoodleLaughGuy from "../components/svg/SvgDoodleLaughGuy";
 
-const text = `Wait...you actually want to know more about me? I must say, I'm flattered!
-
-Well, if you MUST know, I'm an American-Canadian self-taught programmer, developer, graphics designer, computer geek, IT guy, and *checks notes* warm body with a pulse.
-
-For the past couple of years I've been studying and refining my coding skills. Through it all, I ended up writing quite a bit of software in some form or another. I found I rather enjoyed it, especially whenever I injected my own bits of humor and creativity into the mix. After all, I believe if you're going to code something, you might as well have fun with it and amuse yourself - and others, too!
-
-Right now, I'm just livin' and chillin' both on and off the computer. That said, I'm available for hire to work on some cool projects or other tech-related things. Feel free to check out my GitHub to see some of my works, or poke around. Enjoy!`
-
 export default function PageAbout() {
   const noAnim = usePrefersReducedMotion()
-  const useSplitText = useBreakpointValue({ base: false, xl: true })
   return (
     <>
       <PageHeader title="ABOUT" />
@@ -46,35 +26,23 @@ export default function PageAbout() {
               initial={noAnim ? undefined : 'initial'}
               animate={noAnim ? undefined : 'active'}
             >
-              {useSplitText ?
-                <SplitText
-                  text={text}
-                  as={motion.span}
-                  variants={{
-                    initial: {
-                      opacity: 0
-                    },
-                    active: {
-                      opacity: 1,
-                      transition: { duration: 1 }
-                    }
-                  }}
-                /> :
-                <Text
-                  whiteSpace='pre-line'
-                  as={motion.p}
-                  initial={noAnim ? undefined : { opacity: 0 }}
-                  animate={noAnim ? undefined : {
-                    opacity: 1,
-                    transition: {
-                      duration: 2,
-                      delay: 2
-                    }
-                  }}
-                >
-                  {text}
-                </Text>
-              }
+              <Text
+                as={motion.p}
+                initial={noAnim ? undefined : { opacity: 0 }}
+                animate={noAnim ? undefined : {
+                  opacity: 1,
+                  transition: {
+                    duration: 2,
+                    delay: 2
+                  }
+                }}
+              >
+                Wait...you actually want to know more about me? I must say, I'm flattered!
+                <br /><br />
+                Well, if you MUST know, I'm an American-Canadian self-taught programmer, developer, graphics designer, computer geek, IT guy, and *checks notes* warm body with a pulse.
+                <br /><br />
+                Right now, I'm just livin' and chillin' both on and off the computer. That said, I'm available for hire to work on some cool projects or other tech-related things. Feel free to check out my GitHub to see some of my works, or poke around. Enjoy!
+              </Text>
             </Box>
             <Button
               height='auto'
