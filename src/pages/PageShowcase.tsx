4f033c145a7df96bd1e266b62a59b98eb6e8362a
cwylycode <joeisnomoe@mail.com>
2022-08-15 21:11:23 -0600

    Added more showcase cards

--- src/pages/PageShowcase.tsx
+++ src/pages/PageShowcase.tsx
@@ -1,28 +1,59 @@
 import { Container, Flex, usePrefersReducedMotion } from "@chakra-ui/react";
 import PageHeader from "../components/PageHeader";
 import ShowcaseCard, { ShowcaseCardProps } from "../components/ShowcaseCard";
-import img_react_tac_toe from '../images/react_tac_toe.jpg'
+
+import img_my_site from '../images/my_site.jpg'
 import img_pytris from '../images/pytris.jpg'
+import img_rpi from '../images/rpi.jpg'
+import img_dad_site from '../images/dad_site.jpg'
+import img_react_tac_toe from '../images/react_tac_toe.jpg'
 
 const cards: Array<ShowcaseCardProps> = [
   {
-    title: 'React-Tac-Toe',
-    img: img_react_tac_toe,
-    imgOffset: { x: 25, y: 64 },
-    imgSize: 600,
-    link: 'https://cwylycode.github.io/react-tac-toe',
-    linkText: 'Visit Website',
-    text: 'My take on the famous tic-tac-toe game.\n\nThis was my first React project - it was either do a tic-tac-toe game, or make a shopping list (both ideas are popular first-time React projects). Made with bootstrap for handling css.'
+    title: 'My Website',
+    img: img_my_site,
+    imgOffset: { x: 40, y: 0 },
+    imgSize: 750,
+    link: 'https://cwylycode.github.io',
+    linkText: 'Visit Websi-oh you\'re already here...',
+    text: `My "professional" developer portfolio website. Made with React, Chakra UI, and a heap of libraries.\n\nThis was fun (and funny) to make and I learned quite a bit about component frameworks and handling svg files/data. Also, I find it amusing that I'm showcasing my website on my website. 😝`
   },
   {
     title: 'Pytris',
     img: img_pytris,
     imgOffset: { x: 40, y: 350 },
     imgSize: 700,
-    link: 'https://github.com/pytris',
-    linkText: 'Check it on GitHub',
-    text: 'Test text.'
-  }
+    link: 'https://github.com/cwylycode/pytris',
+    linkText: 'Check it out on GitHub!',
+    text: `This is a "Tetris" clone made with the Python programming language.\n\nIt was fun and challenging to code, test and perfect, but I'd say it turned out rather well (both as a project and as an actual, playable game).`
+  },
+  {
+    title: 'RaspberryPi Electronics Projects',
+    img: img_rpi,
+    imgOffset: { x: 0, y: 0 },
+    imgSize: 750,
+    link: 'https://github.com/cwylycode/RPI_Electronics_Projects',
+    linkText: 'Check it out on GitHub!',
+    text: `Bought a RaspberryPi kit one day. I used it originally for running a server and doing Linux-related tasks, but then I started playing around with the electronics portion of it and ended up making a few cool projects in Python.\n\nRight now it's a paper weight, but hopefully I'll make some more stuff with it soon!`
+  },
+  {
+    title: 'My Dad\'s Website',
+    img: img_dad_site,
+    imgOffset: { x: 30, y: 15 },
+    imgSize: 700,
+    link: 'https://richardwyly.com',
+    linkText: 'Visit Website!',
+    text: `Yeah, yeah... I built my dad a Wordpress site. But hey, what was he gonna do, hire an "actual" web developer? 🤔 Not likely! Not when he has a son who'll do it for him at way above market rates!\n\n(Don't tell him 🤫)`
+  },
+  {
+    title: 'React-Tac-Toe',
+    img: img_react_tac_toe,
+    imgOffset: { x: 25, y: 64 },
+    imgSize: 600,
+    link: 'https://cwylycode.github.io/react-tac-toe',
+    linkText: 'Visit Website!',
+    text: `My take on the famous tic-tac-toe game.\n\nThis was my first React project - it was either do a tic-tac-toe game, or make a shopping list (both ideas are popular first-time React projects). Made with bootstrap for handling css.`
+  },
 ]
 
 export default function PageShowcase() {
