b6a605587fd008bee5f8772dab37eaf8ed27e7ae
cwylycode <joeisnomoe@mail.com>
2022-08-24 14:21:58 -0600

    Finished contact page

---
+++ src/components/svg/SvgDoodlePointGuy.tsx
@@ -0,0 +1,81 @@
+import { SvgContainer, SvgContainerProps } from "../SvgContainer";
+
+export default function SvgDoodlePointGuy({ ...props }: SvgContainerProps) {
+  return (
+    <SvgContainer variant='themed' {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        shapeRendering="geometricPrecision"
+        textRendering="geometricPrecision"
+        viewBox="0 0 600 300"
+      >
+        <path vectorEffect='non-scaling-stroke'
+          fill="none"
+          stroke="#3f5787"
+          strokeWidth="0.6"
+          d="M69.763 102.06c22.967 0 104.497-9.788 119.516 2.013 5.01 3.937 47.019 29.126 45.692 31.558-4.799 8.798-37.083 30.946-46.363 35.587-8.365 4.182-85.548-2.015-100.716-2.015-15.062 0-29.907 1.343-44.987 1.343-1.119 0-2.857 1.001-3.357 0s1.266-1.854 2.014-2.686c9.74-10.823 22.54-17.993 34.244-26.186 2.115-1.48 5.009-2.74 3.357-6.043-3.078-6.156-42.188-27.073-41.548-31.558.192-1.342 29.22-.843 32.148-2.014"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792) matrix(1.6693 0 0 2.0256 16.23 -124.342)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="none"
+          stroke="#3f5787"
+          strokeWidth="1.2"
+          d="M148.376 154.895c-17.588-3.518-79.709-2.61-83.852 22.25"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="#070707"
+          strokeWidth="1.2"
+          d="M63.903 177.18c-4.288-1.073-8.989 4.13-4.998 8.12 5.408 5.41 12.359-2.947 6.872-8.433"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="none"
+          stroke="#3f5787"
+          strokeWidth="1.2"
+          d="M162.947 218.395c-10.283 1.285-33.997 3.945-41.315 12.89-6.899 8.432 1.473 20.934-2.967 28.925-3.712 6.682-33.637 2.967-41.533 2.967M250.873 223.736c45.421 6.876 30.073 34.178 37.631 37.957 1.824.912 3.93-1.107 5.934-1.483 13.526-2.536 25.727-1.483 39.307-1.483"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="#d2dbed"
+          strokeWidth="0"
+          d="M-11.263 0c0-6.22 5.043-11.263 11.263-11.263S11.263-6.22 11.263 0 6.22 11.263 0 11.263-11.263 6.22-11.263 0z"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792) translate(225.621 126.115)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="#d2dbed"
+          strokeWidth="0"
+          d="M-11.263 0c0-6.22 5.043-11.263 11.263-11.263S11.263-6.22 11.263 0 6.22 11.263 0 11.263-11.263 6.22-11.263 0z"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792) translate(274.821 126.115)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="none"
+          stroke="#3f5787"
+          strokeWidth="1.2"
+          d="M214.73 152.076c19.035 6.345 33.627 10.095 53.918 3.851 3.418-1.052 7.267-6.14 10.014-3.851 4.27 3.558-2.132 17.358-3.851 20.797-26.583 53.165-68.807-4.088-60.453-20.797"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="none"
+          stroke="#3f5787"
+          strokeWidth="3"
+          d="M408.653 150h52.657"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="#050505"
+          strokeWidth="1.2"
+          d="M460.29 149.24c5.812-2.906 23.209-1.941 16.365 8.813-5.56 8.738-19.11 1.07-15.358-8.309"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792)"
+        ></path>
+        <path vectorEffect='non-scaling-stroke'
+          fill="none"
+          stroke="#3f5787"
+          strokeWidth="1.2"
+          d="M474.857 149.848c11.33-2.518 23.751-1.92 35.34-1.92"
+          transform="matrix(1.27946 0 0 1.27946 -63.106 -66.792)"
+        ></path>
+      </svg>
+    </SvgContainer>
+  )
+}
--- src/pages/PageContact.tsx
+++ src/pages/PageContact.tsx
@@ -1,77 +1,72 @@
-import { Box, Button, Container, FormControl, FormLabel, Heading, Input, InputGroup, InputLeftElement, Text, Textarea, usePrefersReducedMotion, VStack } from "@chakra-ui/react";
-import { BsPerson } from "react-icons/bs";
-import { BiPaperPlane } from 'react-icons/bi'
-import { MdOutlineMail } from 'react-icons/md'
+import { Box, Button, Container, Heading, Text, usePrefersReducedMotion } from "@chakra-ui/react";
 import PageHeader from "../components/PageHeader";
-import useThemed from "../hooks/use-themed";
+import { motion } from "framer-motion";
+import SvgDoodlePointGuy from "../components/svg/SvgDoodlePointGuy";
 
 export default function PageContact() {
   const noAnim = usePrefersReducedMotion()
   return (
     <>
       <PageHeader title="CONTACT" />
-      <Container maxWidth='5xl' paddingY='12'>
-        <Box marginBottom='8' textAlign='center'>
+      <Container position='relative' maxWidth='5xl' paddingY='12' centerContent>
+        <Box
+          as={motion.div}
+          marginBottom='8'
+          textAlign='center'
+          initial={noAnim ? undefined : { opacity: 0, y: -30 }}
+          animate={noAnim ? undefined : { opacity: 1, y: 0, transition: { delay: 1 } }}
+        >
           <Heading color='themed.accent3'>Throw some words at my face!</Heading>
-          <Text marginTop='4' fontSize='xl'>Use the form below to send me an email.</Text>
-        </Box>
-
-        <Box marginTop='10' marginX='auto' width='min-content' maxWidth='100%' >
-          <Box
-            backgroundColor='themed.accent1'
-            borderRadius="3xl"
-            padding='10'
-            width='md'
-            maxWidth='100%'
-          >
-            <VStack spacing={5}>
-              <FormControl id="name">
-                <FormLabel>Your Name</FormLabel>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<BsPerson color="gray.800" />}
-                  />
-                  <Input type="text" size="md" />
-                </InputGroup>
-              </FormControl>
-              <FormControl id="name">
-                <FormLabel>Your Email</FormLabel>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<MdOutlineMail color="gray.800" />}
-                  />
-                  <Input type="text" size="md" />
-                </InputGroup>
-              </FormControl>
-              <FormControl id="name">
-                <FormLabel>Your Message</FormLabel>
-                <Textarea
-                  borderColor="gray.300"
-                  _hover={{
-                    borderRadius: 'gray.300',
-                  }}
-                  placeholder="message"
-                />
-              </FormControl>
-              <FormControl id="name">
-                <Button
-                  // isLoading
-                  loadingText="Sending..."
-                  leftIcon={<BiPaperPlane />}
-                  width='100%'
-                >
-                  Send Message
-                </Button>
-              </FormControl>
-            </VStack>
-          </Box>
-
-          <Text marginTop='5' fontSize='xs' textAlign='center'>
-            * Relax. Your data won't be collected, stored in a database, sold to a third party, used for spam, butchered, mischaracterized, eaten, or used in some nefarious plot.
+          <Text marginTop='4' fontSize='xl'>
+            The best way to get in touch with me is through LinkedIn. Click the button below to go to my profile and send me a message!
           </Text>
         </Box>
+
+        <Button
+          href="https://www.linkedin.com/in/cwylycode"
+          textAlign='center'
+          height='auto'
+          maxWidth='max-content'
+          whiteSpace='break-spaces'
+          marginTop='20'
+          paddingY={{ base: '5', sm: '10' }}
+          _hover={noAnim ? {} : undefined}
+          as={motion.a}
+          initial={noAnim ? undefined : { opacity: 0, y: 30 }}
+          animate={noAnim ? undefined : { opacity: 1, y: 0, transition: { delay: 1.5 } }}
+          whileHover={noAnim ? undefined : {
+            scale: [1, 1.5],
+            transition: { repeat: Infinity, repeatType: 'mirror' }
+          }}
+        >
+          Connect with me on LinkedIn!
+        </Button>
+
+        <Box
+          as={motion.div}
+          initial={noAnim ? undefined : { opacity: 0, x: -50 }}
+          animate={noAnim ? undefined : { opacity: 1, x: 0, transition: { delay: 2 } }}
+          display={{ base: 'none', xl: 'unset' }}
+          position='absolute'
+          bottom='10'
+          left='0'
+          width='64'
+        >
+          <SvgDoodlePointGuy />
+        </Box>
+        <Box
+          as={motion.div}
+          initial={noAnim ? undefined : { opacity: 0, x: 50 }}
+          animate={noAnim ? undefined : { opacity: 1, x: 0, transition: { delay: 2 } }}
+          display={{ base: 'none', xl: 'unset' }}
+          position='absolute'
+          bottom='10'
+          right='0'
+          width='64'
+        >
+          <SvgDoodlePointGuy transform='rotateY(180deg)' />
+        </Box>
+
       </Container>
     </>
   )
