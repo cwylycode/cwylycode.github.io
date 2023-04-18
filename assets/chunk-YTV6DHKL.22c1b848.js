709af84c77ab470c14b7d0c978e2673e69b6c449
cwylycode <joeisnomoe@mail.com>
2023-04-18 17:35:55 -0600

    Updates

---
+++ assets/chunk-YTV6DHKL.22c1b848.js
@@ -0,0 +1,1 @@
+import{f as t,t as o,v as c,j as l,c as k,b as m}from"./index.0c18ebab.js";var f=t(function(a,e){const n=o("Link",a),{className:r,isExternal:s,...i}=c(a);return l(k.a,{target:s?"_blank":void 0,rel:s?"noopener":void 0,ref:e,className:m("chakra-link",r),...i,__css:n})});f.displayName="Link";export{f as L};
